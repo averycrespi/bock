530c7c638a75dd8447c9c2e53a2350e59d721783
Avery Crespi <averycrespi@gmail.com>
2020-04-03 16:57:40 -0700

    Refactor App into ContentGrid

--- src/component/App.js
+++ src/component/App.js
@@ -1,67 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Grid } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-import LabelledList from "./LabelledList";
-import DetailsCard from "./DetailsCard";
-import ObservationChart from "./ObservationChart";
+import ContentGrid from "./ContentGrid";
 
-import {
-  fetchGroups,
-  fetchGroupDetails,
-  fetchSeriesDetails,
-  fetchObservations,
-} from "../logic/api";
-
+import { fetchGroups } from "../logic/api";
 import { PALETTE } from "../style/palette";
 
 const theme = createMuiTheme({ palette: PALETTE });
 
 export default function App() {
   const [groups, setGroups] = useState([]);
-  const [groupDetails, setGroupDetails] = useState({});
-  const [seriesDetails, setSeriesDetails] = useState({});
-  const [observations, setObservations] = useState([]);
 
-  // Load groups once when the component mounts.
+  // Load groups when the component mounts.
   useEffect(() => fetchGroups(setGroups), []);
-
-  const handleGroupClick = (group) => {
-    console.log("Clicked group: " + group.label);
-    fetchGroupDetails(group.name, setGroupDetails);
-    // Clear series details and observations when a new group is clicked.
-    setSeriesDetails({});
-    setObservations([]);
-  };
-
-  const handleSeriesClick = (series) => {
-    console.log("Clicked series: " + series.label);
-    fetchSeriesDetails(series.name, setSeriesDetails);
-    fetchObservations(series.name, setObservations);
-  };
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container>
-        <Grid item xs={3}>
-          <LabelledList items={groups} onClick={handleGroupClick} />
-        </Grid>
-        <Grid item xs={3}>
-          <DetailsCard details={groupDetails} />
-          <LabelledList
-            items={groupDetails.series || []}
-            onClick={handleSeriesClick}
-            maxHeight="50vh"
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <DetailsCard details={seriesDetails} />
-          <ObservationChart
-            details={seriesDetails}
-            observations={observations}
-          />
-        </Grid>
-      </Grid>
+      <ContentGrid groups={groups} />
     </ThemeProvider>
   );
 }
---
+++ src/component/ContentGrid.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Grid } from "@material-ui/core";
+
+import LabelledList from "./LabelledList";
+import DetailsCard from "./DetailsCard";
+import ObservationChart from "./ObservationChart";
+
+import {
+  fetchGroupDetails,
+  fetchSeriesDetails,
+  fetchObservations,
+} from "../logic/api";
+
+export default function ContentGrid(props) {
+  const [groupDetails, setGroupDetails] = useState({});
+  const [seriesDetails, setSeriesDetails] = useState({});
+  const [observations, setObservations] = useState([]);
+
+  const handleGroupClick = (group) => {
+    console.log("Clicked group: " + group.label);
+    fetchGroupDetails(group.name, setGroupDetails);
+    // Clear previous series details and observations.
+    setSeriesDetails({});
+    setObservations([]);
+  };
+
+  const handleSeriesClick = (series) => {
+    console.log("Clicked series: " + series.label);
+    fetchSeriesDetails(series.name, setSeriesDetails);
+    fetchObservations(series.name, setObservations);
+  };
+
+  if (Object.keys(props.groups).length == 0) {
+    return null;
+  } else {
+    return (
+      <Grid container>
+        <Grid item xs={3}>
+          <LabelledList items={props.groups} onClick={handleGroupClick} />
+        </Grid>
+        <Grid item xs={3}>
+          <DetailsCard details={groupDetails} />
+          <LabelledList
+            items={groupDetails.series || []}
+            onClick={handleSeriesClick}
+            maxHeight="50vh"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <DetailsCard details={seriesDetails} />
+          <ObservationChart
+            details={seriesDetails}
+            observations={observations}
+          />
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+ContentGrid.propTypes = {
+  groups: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      label: PropTypes.string,
+      link: PropTypes.string,
+    })
+  ).isRequired,
+};
